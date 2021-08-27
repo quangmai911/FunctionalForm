f0a8e0a3631521aec6664055d1c3d20eb1e2c3db
Quang Mai <quangmai911@gmail.com>
2021-08-27 23:13:50 +1200

    Update Status style in JS

--- main.js
+++ main.js
@@ -11,9 +11,11 @@ var form = document.getElementById("my-form");
             'Accept': 'application/json'
         }
       }).then(response => {
+        status.classList.add('success')
         status.innerHTML = "Thanks for your submission!";
         form.reset()
       }).catch(error => {
+        status.classList.add('error')
         status.innerHTML = "Oops! There was a problem submitting your form"
       });
     }
