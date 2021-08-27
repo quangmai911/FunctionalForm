604a397c49f1581b4569601f9cf26f0c10ba392b
Quang Mai <quangmai911@gmail.com>
2021-08-27 22:57:58 +1200

    Update JS

--- main.js
+++ main.js
@@ -1,46 +1,20 @@
-window.addEventListener("DOMContentLoaded", function () {
-
-  // get the form elements defined in your form HTML above
-
-  var form = document.getElementById("test-form");
-  var button = document.getElementById("test-form-submit");
-  var status = document.getElementById("status");
-
-  // Success and Error functions for after the form is submitted
-
-  function success() {
-    form.reset();
-    status.classList.add('success');
-    status.innerHTML = "Thanks!";
-  }
-
-  function error() {
-    status.classList.add('error');
-    status.innerHTML = "Oops! There was a problem.";
-  }
-
-  // handle the form submission event
-
-  form.addEventListener("submit", function (ev) {
-    ev.preventDefault();
-    var data = new FormData(form);
-    ajax(form.method, form.action, data, success, error);
-  });
-});
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
     }
-  };
-  xhr.send(data);
-}
+form.addEventListener("submit", handleSubmit)
