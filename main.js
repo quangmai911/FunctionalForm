9354252fa3b23e33e2bb598461e623d50cc65244
Quang Mai <quangmai911@gmail.com>
2021-08-27 23:10:31 +1200

    Update Form Status in JS

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var form = document.getElementById("my-form");
     
     async function handleSubmit(event) {
       event.preventDefault();
-      var status = document.getElementById("my-form-status");
+      var status = document.getElementById("status");
       var data = new FormData(event.target);
       fetch(event.target.action, {
         method: form.method,
